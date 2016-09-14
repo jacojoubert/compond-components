011c7b1e25dbda404d30e434693c9ed077bc8043
Jaco Joubert <joubert.jaco@gmail.com>
2016-09-14 15:03:05 -0400

    Add error handling to email field. Use __ for descendent class names intead of --.

--- tests/dummy/app/components/form-component.js
+++ tests/dummy/app/components/form-component.js
@@ -1,12 +1,56 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  wasFocused: false,
+  error: false,
+  displayError: 'auto',
+
+  _forceShowError: Ember.computed.equal('displayError', 'always'),
+  _forceHideError: Ember.computed.equal('displayError', 'never'),
+
+  isFocused: false,
+  _wasFocused: false,
+  _previousErrorState: undefined,
+
+  errorState: Ember.computed('error', 'value', '_wasFocused', '_forceShowError', '_forceHideError', function() {
+    if (this.get('_forceHideError')) {
+      return false;
+    }
+
+    const wasFocused = this.get('_wasFocused');
+    const error = this.get('error');
+    const forceShowError = this.get('_forceShowError');
+    const previousErrorState = this.get('_previousErrorState');
+
+    if (error && (wasFocused || previousErrorState || forceShowError)) {
+      return this.set('_previousErrorState', {
+        hasError: true,
+        message: typeof error === 'string' ? error : null
+      });
+
+    } else {
+      this.set('_previousErrorState', undefined);
+    }
+
+    return false;
+  }),
 
   actions: {
+    onFocusIn() {
+      this.setProperties({
+        '_wasFocused': false,
+        'isFocused': true
+      });
+    },
+
+    onFocusOut() {
+      this.setProperties({
+        '_wasFocused': true,
+        'isFocused': false
+      });
+    },
+
     focusIn() {
       this.$().find('input:first').focus();
-      this.set('wasFocused', true);
     }
   }
 });
--- tests/dummy/app/components/form-email.js
+++ tests/dummy/app/components/form-email.js
@@ -2,5 +2,13 @@ import FormComponent from './form-component';
 import layout from '../templates/components/form-email';
 
 export default FormComponent.extend({
-  layout
+  layout,
+
+  value: undefined,
+
+  error: Ember.computed('value', function() {
+    if (!this.get('value')) {
+      return "You must enter an email address."
+    }
+  })
 });
