9e10726e83cd91e1ac989c408cdb7ebc47a366db
Jaco Joubert <joubert.jaco@gmail.com>
2016-09-15 16:26:18 -0400

    Fix formatting of income field.

--- tests/dummy/app/components/form-income.js
+++ tests/dummy/app/components/form-income.js
@@ -5,23 +5,33 @@ import layout from '../templates/components/form-income';
 export default FormComponent.extend({
   layout,
 
-  didInsertElement() {
-    const field = new FieldKit.TextField(this.$().find('input')[0],
-                    new FieldKit.NumberFormatter()
-                      .setNumberStyle(FieldKit.NumberFormatter.Style.CURRENCY)
-                      .setLocale('en-US')
-                      .setCountryCode('US')
-                      .setCurrencyCode('USD')
-                      .setMaximumFractionDigits(0));
-    field.setFocusedPlaceholder('$');
-    field.setUnfocusedPlaceholder('$');
-  },
-
   value: undefined,
 
   error: Ember.computed('value', function() {
     if (!this.get('value')) {
       return "You must enter an income."
+    }
+  }),
+
+  conditionalValue: Ember.computed('value', 'isFocused', {
+    get(key) {
+      if (this.get('isFocused')) {
+        return this.get('value');
+
+      } else {
+        const formatter = new FieldKit.NumberFormatter()
+                            .setNumberStyle(FieldKit.NumberFormatter.Style.CURRENCY)
+                            .setLocale('en-US')
+                            .setCountryCode('US')
+                            .setCurrencyCode('USD')
+                            .setMaximumFractionDigits(0);
+        const value = (this.get('value') || '').replace(/\D/g,'');
+        return formatter.format(value);
+      }
+    },
+
+    set(key, value) {
+      return this.set('value', value.replace(/\D/g,''));
     }
   })
 });
